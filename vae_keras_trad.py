3e40ea607f1b430beea0e30b5947a18b536729af
theophile <theophile.dupre@atiam.fr>
2018-12-05 00:19:40 +0100

    VAE fonctionnel, affiche des exemples de données reconstituées et de données générées

--- vae_keras_trad.py
+++ vae_keras_trad.py
@@ -9,13 +9,15 @@ Created on Tue Nov 27 19:34:36 2018
         correctement (loss converge vers -10 ?)
         En plus l'image reconstituée est toujours identique (une sorte de 9 très floue)
 """
-
+import os
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader 
 import matplotlib.pyplot as plt
+from torchvision.utils import save_image
 
 #%% Loading data
 '''
@@ -25,19 +27,23 @@ Les objets DataLoader permettent d'organiser les fichiers en batch et de pouvoir
 y accéder facilement pour l'entraînement
 Pour l'instant, le test_dataset n'est pas utilisé
 '''
-batch_size = 10
+batch_size = 512
 data_dir = 'data'
 train_dataset = datasets.MNIST(data_dir, train=True, download=True, 
                     transform=transforms.Compose([transforms.ToTensor(),
                     lambda x: x > 0, # binarisation de l'image
-                    lambda x: x.float(),
-                    transforms.Normalize((0.1307,), (0.3081,)),
-                    ]))
+                    lambda x: x.float()]))
 test_dataset = datasets.MNIST(data_dir, train=False, download=True, 
                     transform=transforms.Compose([transforms.ToTensor(),
-                    transforms.Normalize((0.1307,), (0.3081,))]))
+                    lambda x: x > 0, # binarisation de l'image
+                    lambda x: x.float()]))
 train_loader = DataLoader(train_dataset,batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(test_dataset,batch_size=batch_size, shuffle=True)
+
+# Creation d'un dossier results/ pour stocker les resultats
+results_dir = 'results/'
+if not os.path.exists(results_dir):
+ os.makedirs(results_dir)
 
 #%% Définition des modules du VAE : Encoder, Z_sampling, Decoder
 
@@ -78,7 +84,7 @@ class VAE(nn.Module):
     def reparametrize(self, mu,logvar):
         
         std = torch.exp(0.5*logvar)
-        eps = torch.randn_like(std)
+        eps = torch.randn_like(std, requires_grad = True)
         
         return eps.mul(std).add_(mu)
     
@@ -96,7 +102,7 @@ def vae_loss(x, x_sample, mu, logvar):
     return recons_loss(x_sample, x) + KL_div
     
 def train_vae(epoch):
-    
+    train_loss = 0
     vae.train()
     for batch_idx, (data, _) in enumerate(train_loader):
         optimizer.zero_grad()
@@ -104,6 +110,7 @@ def train_vae(epoch):
         
         x_approx, mu, logvar = vae(data)
         loss = vae_loss(data, x_approx, mu, logvar)
+        train_loss += loss.item()
         loss.backward()
         optimizer.step()
         
@@ -112,11 +119,13 @@ def train_vae(epoch):
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.item()))
-            display_data(batch_size - 1, x_approx, data)
+            
+    print('====> Epoch: {} Average loss: {:.4f}'.format(
+          epoch, train_loss*N / len(train_loader.dataset)))
         
         
 def test_vae(epoch):
-    
+    test_loss = 0
     vae.eval()
     with torch.no_grad():
         for batch_idx, (data, target) in enumerate(test_loader):
@@ -125,78 +134,43 @@ def test_vae(epoch):
             
             x_approx, mu, logvar = vae(data)
             loss = vae_loss(x_approx, data, mu, logvar)
+            test_loss += loss.item()
             
-            # Affichage de la loss tous les 100 batchs
-            if batch_idx % 10 == 0:
-                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                    epoch, batch_idx * len(data), len(train_loader.dataset),
-                    100. * batch_idx / len(train_loader), loss.item()))
-                display_data(batch_size - 1, x_approx, data)
-                
-    
-'''
-Fonction d'affichage d'une image et de sa reconstruction
-@param: data_id: id entre 0 et batch_size
-'''
-def display_data(data_id, x_approx, data):
-        approx_data = x_approx[data_id].detach().numpy().reshape((28,28))
-        data = data[data_id].detach().numpy().reshape((28,28))
-        plt.figure
-        plt.subplot(1,2,1)
-        plt.imshow(data, cmap='gray')
-        plt.xlabel('Original data')
-        plt.subplot(1,2,2)
-        plt.imshow(approx_data, cmap='gray')
-        plt.xlabel('Reconstructed data')        
-    
+            # Sauvegarde d'exemples de données reconstituées avec les données d'origine
+            if batch_idx == 0:
+                n = min(data.size(0), 8)
 
+                comparison = torch.cat([data.view(N, 1, 28, 28)[:n],
+                                      x_approx.view(N, 1, 28, 28)[:n]])
+                save_image(comparison.cpu(),
+                           'results/reconstruction_' + str(epoch) + '.png', nrow=n)
 
+    test_loss /= len(test_loader.dataset)/N
+    print('====> Test set loss: {:.4f}'.format(test_loss))
+            
+    
 #%% Instanciation du VAE
 
 vae = VAE(D_in, D_enc, D_z, D_dec, D_out)
-optimizer = torch.optim.Adam(vae.parameters(), 1e-4)
+optimizer = torch.optim.Adam(vae.parameters(), 1e-3)
 
 #%% Training loop
 
-# Itération du modèle sur 5 epoches
-for epoch in range(5): 
-    
-    
+# Itération du modèle sur 50 epoches
+for epoch in range(50): 
     train_vae(epoch)
     test_vae(epoch)
     
-    
-    '''
-    # parcourt de tous les batchs à chaque epoch
-    for batch_idx, (data, target) in enumerate(train_loader): 
-        # target n'est pas utilisé car la loss s'évalue par rapport à la donnée 
-        # de départ et non par rapport à la classe de la donnée
-        X = data.reshape((N,D_in)) # Image (28,28) tranformée en vecteur (784,1)
-        mu, var = encoder.forward(X) # Encodage
-        z = sample_z(mu, var) # Tirage d'une valeur z dans l'espace latent
-        X_sample = decoder.forward(z) # Décodage
-        
-        # Loss = erreur de reconstruction de la donnée + KL-divergence
-        # KL-div : Calcul de la ressemblance entre la distribution réel P(X|Z) (intraçable)
-        #           et de son approximation Q(X|Z) calculée par le vae
-        kl_loss = 0.5 * torch.sum(torch.exp(var) + mu**2 - 1. - var) 
-        loss = loss_recons(X_sample, X) + kl_loss
-        
-        # Mise à zero des gradients 
-        optimizer_enc.zero_grad()
-        optimizer_dec.zero_grad()
-        
-        # Retropropagation
-        loss.backward()
-        
-        # Mise à jour des poids
-        optimizer_enc.step()
-        optimizer_dec.step()
-        
-        # Affichage de la loss tous les 100 batchs
-        if batch_idx % 10 == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.item()))
-            display_data(batch_size - 1)
-    '''    
+    # Sauvegarde d'exemples de données générées à partir de l'espace latent
+    with torch.no_grad():
+        #sample = torch.randn(64, D_z)
+        Nd = 8
+        sample_x, sample_y = np.meshgrid(4*np.linspace(0,1,Nd)-2,4*np.linspace(0,1,Nd)-2)
+        sample_x = sample_x.reshape(Nd**2,1)
+        sample_y = sample_y.reshape(Nd**2,1)
+        sample = np.concatenate((sample_x,sample_y),axis=1)
+        sample = torch.from_numpy(sample).type(torch.float)
+        sample = vae.decoder(sample)
+        save_image(sample.view(Nd**2, 1, 28, 28), 
+'results/sample_' + str(epoch) + '.png')
+      
